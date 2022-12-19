7a95e63de821208db7b67c902fb55503bac85345
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-18 21:50:22 -0600

    Selection sort done.

---
+++ 2-selection_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+    unsigned long i, j, index_of_min;
+    int temp_buff;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        index_of_min = i;
+
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[index_of_min])
+                index_of_min = j;
+        }
+
+        temp_buff = array[index_of_min];
+        array[index_of_min] = array[i];
+        array[i] = temp_buff;
+
+        print_array(array, size);
+    }
+}
--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
 void quick_s(int *array, int lo, int hi, size_t size);
+void selection_sort(int *array, size_t size);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
