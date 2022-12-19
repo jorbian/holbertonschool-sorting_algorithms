64353fbd2819f5d20ff83db22d0899cc3c5601bc
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-18 21:51:22 -0600

    Fixed betty.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,23 +2,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-    unsigned long i, j, index_of_min;
-    int temp_buff;
+	unsigned long i, j, index_of_min;
+	int temp_buff;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        index_of_min = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		index_of_min = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[index_of_min])
-                index_of_min = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index_of_min])
+				index_of_min = j;
+		}
 
-        temp_buff = array[index_of_min];
-        array[index_of_min] = array[i];
-        array[i] = temp_buff;
+		temp_buff = array[index_of_min];
+		array[index_of_min] = array[i];
+		array[i] = temp_buff;
 
-        print_array(array, size);
-    }
+		print_array(array, size);
+	}
 }
